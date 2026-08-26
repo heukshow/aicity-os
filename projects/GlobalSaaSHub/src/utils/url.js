9f06ae3f560daf918b45d10ae7ca76538a8e6591
heukshow <qmfforfhem@gmail.com>
2026-08-27 08:23:52 +0900

    fix(GlobalSaaSHub): add Fireflies affiliate override

--- projects/GlobalSaaSHub/src/utils/url.js
+++ projects/GlobalSaaSHub/src/utils/url.js
@@ -5,6 +5,7 @@
 const VERIFIED_AFFILIATE_OVERRIDES = {
   castmagic: "https://castmagic.io?fpr=sangkwon-an54",
   descript: "https://get.descript.com/ole5fu20j5sq",
+  fireflies-ai: "https://fireflies.ai/?fpr=sangkwon53",
   vidiq: "https://vidiq.com/coshuma",
 };
 
